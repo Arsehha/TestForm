ac6ecde5c8b6117b9c39c2b036cdc9fbf1cb417d
oleg <oleg_zub00@mail.ru>
2024-12-04 12:46:07 +0300

    plus content and some fixes

--- server/config.ts
+++ server/config.ts
@@ -1,11 +1,29 @@
 const config: any = {
-    port:4000,
-    DB_HOST: "localhost",
+    is_prod: true,
+    port: 4000,
+    // DB
     DB_PORT: 5432,
-    DB_USERNAME: "postgres",
-    DB_PASSWORD: "1234root",
-    DB_DATABASE: "testForm",
+    DB_HOST: dbHost(),
+    DB_USERNAME: dbUsername(),
+    DB_PASSWORD: dbPassword(),
+    DB_DATABASE: dbDATABASE(),
     DB_TYPE: "postgres",
+}
+
+function dbHost(): string {
+    return config.is_prod ? '' : 'localhost'
+}
+
+function dbUsername(): string {
+    return config.is_prod ? '' : 'postgres'
+}
+
+function dbPassword(): string {
+    return config.is_prod ? '' : '1234root'
+}
+
+function dbDATABASE(): string {
+    return config.is_prod ? '' : 'testForm'
 }
 
 export default config
